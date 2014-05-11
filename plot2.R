fae3b98577c5f7535bbc2134c8bf9a6102ebfac9
Halan Jimenez <halan.jimenez@gmail.com>
2014-05-11 03:24:42 -0500

    Uploading Assignment 1

--- plot2.R
+++ plot2.R
@@ -1,8 +1,11 @@
+##-------Cleaning data-----------
 col_classes = c("character", "character", rep("numeric", 7))
 data <- read.csv("household_power_consumption.txt", colClasses = col_classes, na.strings=c("?"), sep=";")
 data <- within(data, DateTime <- paste(Date, Time, sep=" "))
+data <- data[(data$Date == "1/2/2007") | (data$Date == "2/2/2007"),]
 data$DateTime = as.POSIXct(strptime(data$DateTime, format = "%d/%m/%Y %H:%M:%S"))
-data = subset(data, DateTime >= as.POSIXct("2007-02-01") & DateTime < as.POSIXct("2007-02-03"))
+
+##-------Plot-------
 png(filename="plot2.png", width = 480, height = 480)
-hist(as.numeric(data$Global_active_power), col="black")
+plot(data$DateTime, data$Global_active_power, col="black", type="l", xlab="", ylab="Global Active Power (kilowatts)")
 dev.off()
